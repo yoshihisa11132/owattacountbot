631da7ec61902e41ca5fd9d61b76c42831de91a7
yoshihisa11132 <hhjggcyy@outlook.jp>
2026-04-08 01:40:37 +0900

    最初のcommit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # owattacountbot
 一部で脱AIを頑張りました。
+[実際にホストしてます。](https://discord.com/oauth2/authorize?client_id=1491089714533372095&permissions=274877975552&integration_type=0&scope=bot)
+<hr>
+<h3>機能一覧</h3>
+<p>指定した単語に反応</p>
+<p>反応した回数を確認</p>
+<p>反応しないチャンネルの追加！</p>
+<hr>
+<h3>ホスト方法</h3>
+<p>1. このリポジトリをクローンする</p>
+<p>2. envを追加して編集する</p>
+<p>3. main.pyの気に入らないとこを直すかissueを作る</p>
+<p>4. コードを実行する</p>
+<p>5. 完成！</p>
+<hr>
+<h3>.envのテンプレート</h3>
+<p>github gistに置いてるので都度調整して.envとして保存してください。</p>
+[そのりんく](https://gist.github.com/yoshihisa11132/4f73ad3fbf1a974ed4c1b62c0be0f98c)
+
+
---
+++ main.py
@@ -0,0 +1,157 @@
+# そすこ：https://github.com/yoshihisa11132/owattacountbot
+import discord
+import json
+import os
+import ast
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+prefix = os.getenv('prefix')
+save = os.getenv('save')
+blsave = os.getenv('blsave')
+word = os.getenv('word')
+# MPL-2.0ライセンスによりメインコードを改変した場合は公開が必要です。
+# forkすると便利、、、かも？
+repo = os.getenv('repo')
+
+client = discord.Client(intents=intents)
+
+def bl(serverid,check):
+  serverid = str(serverid)
+  try:
+    with open(blsave, mode='x') as jsonmake:
+       jsonmake.write("[]")
+       jsonmake.close()
+  except FileExistsError:
+    pass
+  data = open(blsave, "r")
+  data2 = ast.literal_eval(data.read())
+  if data2 == "":
+    data.close()
+    data = open(blsave, "w")
+    data.write("[]")
+  data.close()
+  if check == "check":
+    data = open(blsave, "r")
+    data2 = ast.literal_eval(data.read())
+    if serverid in data2:
+      data.close()
+      return False
+    else:
+      data.close()
+      return True
+  else:
+    if check == "plus":
+      if bl(serverid,"check") == False:
+        bl(serverid,"del")
+        return False
+      else:
+        data = open(blsave, "r")
+        data2 = ast.literal_eval(data.read())
+        data2.append(serverid)
+        data.close()
+        data = open(blsave, "w")
+        data.write(str(data2))
+        data.close()
+        return True
+    else:
+      if check == "del":
+        data = open(blsave, "r")
+        data2 = ast.literal_eval(data.read())
+        data2.remove(serverid)
+        data.close()
+        data = open(blsave, "w")
+        data.write(str(data2))
+        data.close()
+
+
+def wordplus(userid):
+  userid = str(userid)
+  try:
+    with open(save, mode='x') as jsonmake:
+       jsonmake.write("{}")
+       jsonmake.close()
+  except FileExistsError:
+    pass
+  data = open(save, "r")
+  try:
+    data2 = json.load(data)
+  except:
+   with open(save, mode='w') as jsonmake:
+      jsonmake.write("{}")
+      jsonmake.close()
+   data.close()
+   data = open(save , "r")
+   data2 = json.load(data)
+  data.close()
+  if data2.get(userid) == None:
+    data2[userid] = 0
+  siniti = int(data2[userid])
+  data2[userid] = siniti + 1
+  with open(save, 'w') as f:
+    json.dump(data2, f, indent=2)
+    f.close()
+  return data2[userid]
+
+def wordcheck(userid):
+  userid = str(userid)
+  try:
+    with open(save, mode='x') as jsonmake:
+       jsonmake.write("{}")
+       jsonmake.close()
+  except FileExistsError:
+    pass
+  data = open(save, "r") #ここが(1)
+  try:
+    data2 = json.load(data)
+  except:
+   with open(save, mode='w') as jsonmake:
+      jsonmake.write("{}")
+      jsonmake.close()
+   data.close()
+   data = open(save, "r") #ここが(1)
+   data2 = json.load(data)
+  data.close()
+  if data2.get(userid) == None:
+    data2[userid] = 0
+  return int(data2[userid])
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author.bot == True:
+        return
+
+    if word in message.content:
+      try:
+        id = message.guild.id
+      except:
+        return
+      if bl(id,"check") == True:
+        noun = wordplus(message.author.id)
+        await message.reply(f"{noun}回{word}って言ったらしいよ\n-# [諸説あり]")
+
+    if message.content.startswith(f"{prefix}check"):
+        noun = wordcheck(message.author.id)
+        await message.reply(f"いままでに{noun}回{word}と言った事があるようですよ？\n-# [諸説あり]")
+
+    if message.content.startswith(f"{prefix}help"):
+        await message.reply(f"{word} - カウントされます\n{prefix}check - あなたのカウントを見れるらしいね\n{prefix}bl - サーバーのカウントを停止するでー\n{prefix}help - こ　れ\n(カウント回数はグローバルです。)\n作者はAIの使用についてこう公表しています。\n使用された箇所：\n・コードの監査や一部エラーの修正\n・replitの自動補完\n使用されていない箇所：\n・大半のコードの作成\n・botのアイコンや名前\n・サーバーのホスティング\n\nこのbotはオープンソースです。\n{repo}")
+
+    if message.content.startswith(f"{prefix}bl"):
+      try:
+        id = message.guild.id
+      except:
+        return
+      if bl(id,"plus") == True:
+          await message.reply(f"サーバーの{word}カウントを停止したよ！")
+      else:
+          await message.reply(f"サーバーの{word}カウントを再開したよ！")
+
+client.run(os.getenv('token'))
